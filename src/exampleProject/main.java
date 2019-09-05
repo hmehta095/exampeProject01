eb09f1b141b3826d8b6b6426ca050147eed04bcd
himanshumehta <himanshumehta@10.51.212.113>
2019-09-05 11:19:21 -0400

    initial commit

---
+++ src/exampleProject/main.java
@@ -0,0 +1,23 @@
+package exampleProject;
+
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		System.out.println("My git hub example");
+		
+		
+		
+		// take input from keyboard
+		System.out.println("Whats your name?");
+		Scanner keyboard = new Scanner(System.in);
+		
+		//1. create a variable to store user input
+		String personName = keyboard.nextLine();
+		
+		//2. output a welcome message
+		System.out.println("Hello " + personName);
+	}
+
+}
